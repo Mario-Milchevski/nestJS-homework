4dc176f56532341a327cec5a4a9ea492b74b4916
mario.milchevski <mario.milchevski@gmail.com>
2024-04-28 13:34:14 +0200

    auth initalize

--- Homework_5_Music_App_ Auth/music_app/src/app.module.ts
+++ Homework_5_Music_App_ Auth/music_app/src/app.module.ts
@@ -3,13 +3,15 @@ import { ArtistModule } from './artists/artist.module';
 import { SongsModule } from './songs/song.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
   }),
     DatabaseModule,
     ArtistModule,
-    SongsModule
+    SongsModule,
+    AuthModule
   ],
   controllers: [],
   providers: [],
